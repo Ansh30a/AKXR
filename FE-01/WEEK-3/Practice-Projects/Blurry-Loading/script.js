351fddf930fb48c16de15a2ddf3c2ae25a73d105
Ansh <anshuman302004@gmail.com>
2025-12-18 20:37:28 +0530

    completed first practice project for FE-01 WEEK-3

--- FE-01/WEEK-3/Practice-Projects/Blurry-Loading/script.js
+++ FE-01/WEEK-3/Practice-Projects/Blurry-Loading/script.js
@@ -14,8 +14,9 @@ function blurFunc() {
 
     loadText.innerText = `${load}%`
     loadText.style.opacity = scale(load, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
-    return (num - in_max) * (out_max - out_min) / (in_max - in_min) + out_min
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
