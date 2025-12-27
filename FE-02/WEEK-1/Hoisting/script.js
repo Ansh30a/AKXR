cc103ddd3c2daa8358f561538bd024af2e418755
Ansh <anshuman302004@gmail.com>
2025-12-28 01:18:03 +0530

    learn: Hoisting done

---
+++ FE-02/WEEK-1/Hoisting/script.js
@@ -0,0 +1,27 @@
+// getName()
+
+console.log(x);
+
+console.log(getName)
+
+var x = 7
+
+// let x = 7 // --------- doesn't work with let
+
+// function getName() {
+//     console.log('AKXR')
+// }
+
+// var getName = () => {
+//     console.log('AKXR')    
+// }
+
+var getName = function() {
+    console.log('AKXR')    
+}
+
+getName()
+
+// console.log(x);
+
+// console.log(getName)
