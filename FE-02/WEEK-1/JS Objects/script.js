9cf3e793a9a92e6bdb9124a8de21fd94aab09241
Ansh <anshuman302004@gmail.com>
2025-12-27 16:33:26 +0530

    learn: start FE-02 WEEK-1

---
+++ FE-02/WEEK-1/JS Objects/script.js
@@ -0,0 +1,15 @@
+let user = {
+    name: 'ansh',
+    age: 30,
+    email: 'ansh@ansh.com',
+    location: 'Delhi',
+    blogs: ['hello', 'bye']
+}
+
+console.log(user)
+
+console.log(user.name)
+
+user.age = 21
+
+console.log(user.age)
