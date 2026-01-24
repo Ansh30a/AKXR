0cb71a6b9310c045417206b46fd5bb29f8b19c05
Ansh <anshuman302004@gmail.com>
2026-01-24 22:50:19 +0530

    learn: - create restaurant cards - create interface for props for dynamic restaurant data - create css designs

--- FE-03/WEEK-2-5/FoodDeliveryApp/src/Components/Body.tsx
+++ FE-03/WEEK-2-5/FoodDeliveryApp/src/Components/Body.tsx
@@ -1,12 +1,13 @@
 import RestaurantCard from "./RestaurantCard";
-// import React from 'react';
+import './Body.css';
 
 const Body = () => {
   return (
     <div className="body">
         <div className="search-bar">Search</div>
         <div className="restaurant-container">
-            <RestaurantCard />
+            <RestaurantCard name="KFC" rating={4.0} cuisine={["North Indian"]} eta="30 mins" />
+            <RestaurantCard name="McD" rating={4.2} cuisine={["Fast Food"]} eta="30 mins" />
         </div>
     </div>
   );
--- FE-03/WEEK-2-5/FoodDeliveryApp/src/Components/RestaurantCard.tsx
+++ FE-03/WEEK-2-5/FoodDeliveryApp/src/Components/RestaurantCard.tsx
@@ -1,9 +1,20 @@
 import './RestaurantCard.css';
 
-const RestaurantCard = () => {
+interface RestaurantCardProps {
+    name: string;
+    cuisine: string[];
+    rating: number;
+    eta: string;
+};
+
+const RestaurantCard = ({ name, cuisine, rating, eta }: RestaurantCardProps) => {
   return (
     <div className="restaurant-card">
-        <h3>Food</h3>
+        <img src="https://images.pexels.com/photos/33428723/pexels-photo-33428723.jpeg" alt="" />
+        <h3>{name}</h3>
+        <h4>{cuisine}</h4>
+        <h4>{rating}</h4>
+        <h4>{eta}</h4>
     </div>
   );
 };
