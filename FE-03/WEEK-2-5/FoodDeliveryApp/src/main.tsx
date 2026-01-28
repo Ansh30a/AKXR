3798c131b4d6e49ebf9c7f16f5760f2227a7ed80
Ansh <anshuman302004@gmail.com>
2026-01-28 18:11:53 +0530

    feat/learn: - error routing in react-router - create custom error page - create contact component

--- FE-03/WEEK-2-5/FoodDeliveryApp/src/main.tsx
+++ FE-03/WEEK-2-5/FoodDeliveryApp/src/main.tsx
@@ -4,6 +4,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import Body from './Components/Body/Body';
 import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
 import './index.css';
 
 const appRouter = createBrowserRouter([
@@ -19,7 +21,12 @@ const appRouter = createBrowserRouter([
                 path: '/about',
                 element: <About />,
             },
+            {
+                path: '/contact',
+                element: <Contact />
+            }
         ],
+        errorElement: <ErrorPage />,
     },
 ]);
 
