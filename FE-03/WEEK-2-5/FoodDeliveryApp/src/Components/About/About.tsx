9d347c05b5a1002998a2ae5e766692862556217b
Ansh <anshuman302004@gmail.com>
2026-01-29 12:00:49 +0530

    feat/learn: - class based components - create user component to render in About page

--- FE-03/WEEK-2-5/FoodDeliveryApp/src/Components/About/About.tsx
+++ FE-03/WEEK-2-5/FoodDeliveryApp/src/Components/About/About.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
+import UserPage from "../UserPage/UserPage";
 import "./About.css";
+import Shimmer from "../Shimmer/Shimmer";
+
+interface GitHubUser {
+    name: string;
+    login: string;
+    avatar_url: string;
+    html_url: string;
+};
 
 const About = () => {
+    const [user, setUser] = useState<GitHubUser | null>(null);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const res = await fetch(`https://api.github.com/users/ansh30a`);
+            const data: GitHubUser = await res.json();
+            setUser(data);
+        };
+
+        fetchUser();
+    }, []);
+
+    if (!user) return <Shimmer />;
+
     return (
         <div>
-            <h1>About</h1>
+            <UserPage name={user.name} username={user.login} avatar_url={user.avatar_url} github_link={user.html_url} />
         </div>
     );
 };
