1b21bb51fb1dcc474b36830db4d27a343d9d7067
Ansh <anshuman302004@gmail.com>
2026-02-03 15:39:54 +0530

    learn/feat(layout): - higher order components (HOC) - create a HOC to display Open label above restaurant cards

--- FE-03/WEEK-2-5/FoodDeliveryApp/src/Components/Body/Body.tsx
+++ FE-03/WEEK-2-5/FoodDeliveryApp/src/Components/Body/Body.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 // import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard/RestaurantCard";
-import resData from '../../utils/RestaurantData.json';
+import withOpenLabel from "../Label/OpenLabel";
+import resData from "../../utils/RestaurantData.json";
 // import Shimmer from "../Shimmer/Shimmer";
-import useOnlineStatus  from "../../Hooks/useOnlineStatus";
-import './Body.css';
+import useOnlineStatus from "../../Hooks/useOnlineStatus";
+import "./Body.css";
 
 interface RestaurantInfo {
     id: string;
@@ -15,24 +16,24 @@ interface RestaurantInfo {
         slaString: string;
     };
     cloudinaryImageId: string;
-};
-
+    isOpen: boolean;
+}
 
 interface Restaurant {
     info: RestaurantInfo;
-};
-
+}
 
 interface RestaurantData {
     restaurants: Restaurant[];
-};
+}
 
 const Body = () => {
-
     const initialData = resData as RestaurantData;
 
     // const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
-    const [restaurants, setRestaurants] = useState<Restaurant[]>(initialData.restaurants);
+    const [restaurants, setRestaurants] = useState<Restaurant[]>(
+        initialData.restaurants,
+    );
 
     const [searchBarText, setSearchBarText] = useState("");
 
@@ -50,60 +51,70 @@ const Body = () => {
     //     return <Shimmer />
     // }
 
+    const RestaurantCardOpen = withOpenLabel(RestaurantCard);
+
     const onlineStatus = useOnlineStatus();
 
     if (onlineStatus === false) {
-        return (
-            <h1>You're Offline!!!</h1>
-        )
+        return <h1>You're Offline!!!</h1>;
     }
 
-    return ( // ---- restaurants.length === 0 ? <Shimmer /> :
+    return (
+        // ---- restaurants.length === 0 ? <Shimmer /> :
         <div className="body">
             <div className="search-bar">
-                <input 
-                    type="text" 
-                    placeholder="Search.." 
-                    className="search-input" 
-                    value={searchBarText} 
-                    onChange={(e) => {setSearchBarText(e.target.value)}} 
+                <input
+                    type="text"
+                    placeholder="Search.."
+                    className="search-input"
+                    value={searchBarText}
+                    onChange={(e) => {
+                        setSearchBarText(e.target.value);
+                    }}
                 />
                 <button
                     onClick={() => {
-                    const filtered = initialData.restaurants.filter(
-                        (res) => res.info.name.toLowerCase().includes(searchBarText.toLowerCase())
-                    );
-                    setRestaurants(filtered);
-                }}                    
+                        const filtered = initialData.restaurants.filter((res) =>
+                            res.info.name
+                                .toLowerCase()
+                                .includes(searchBarText.toLowerCase()),
+                        );
+                        setRestaurants(filtered);
+                    }}
                 >
                     Search
                 </button>
             </div>
             <div className="filter">
-                <button 
-                    className="filter-btn" 
-                        onClick={() => {
-                            const filtered = initialData.restaurants.filter(
-                                (res) => res.info.avgRating >= 4
-                            );
-                            setRestaurants(filtered);
-                        }}
+                <button
+                    className="filter-btn"
+                    onClick={() => {
+                        const filtered = initialData.restaurants.filter(
+                            (res) => res.info.avgRating >= 4,
+                        );
+                        setRestaurants(filtered);
+                    }}
                 >
                     Top Rated Restaurants
                 </button>
             </div>
             <div className="restaurant-container">
-                {restaurants.map((restaurant) => (
-                    <RestaurantCard
-                        key={restaurant.info.id}
-                        id={restaurant.info.id}
-                        name={restaurant.info.name}
-                        cuisine={restaurant.info.cuisines}
-                        rating={restaurant.info.avgRating}
-                        eta={restaurant.info.sla.slaString}
-                        imageId={restaurant.info.cloudinaryImageId}
-                    />
-                ))}
+                {restaurants.map((restaurant) => {
+                    const CardComponent = restaurant.info.isOpen
+                    ? RestaurantCardOpen
+                    : RestaurantCard;
+                    return (
+                        <CardComponent
+                            key={restaurant.info.id}
+                            id={restaurant.info.id}
+                            name={restaurant.info.name}
+                            cuisine={restaurant.info.cuisines}
+                            rating={restaurant.info.avgRating}
+                            eta={restaurant.info.sla.slaString}
+                            imageId={restaurant.info.cloudinaryImageId}
+                        />
+                    );
+                })}
             </div>
         </div>
     );
