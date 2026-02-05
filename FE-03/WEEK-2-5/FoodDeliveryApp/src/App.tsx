72b2850e1a96e8e75241af98190242d411502840
Ansh <anshuman302004@gmail.com>
2026-02-05 12:33:05 +0530

    learn/feat/fix: - global context - minor UI fix

--- FE-03/WEEK-2-5/FoodDeliveryApp/src/App.tsx
+++ FE-03/WEEK-2-5/FoodDeliveryApp/src/App.tsx
@@ -1,18 +1,33 @@
-import { Outlet } from 'react-router-dom';
-import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
-import './App.css';
+import { Outlet } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import { useEffect, useState } from "react";
+import UserContext from "./Context/userContext";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="app-shell">
-      <Header />
-      <main className="app-main">
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
-  );
+    const [userName, setUserName] = useState("");
+
+    // Fake Auth
+    useEffect(() => {
+        // Make an ACTUAL API call here to fetch user data
+        const data = {
+            name: "Ansh",
+        };
+        setUserName(data.name);
+    }, []);
+
+    return (
+        <UserContext.Provider value={{ loggedInUser: userName }}>
+            <div className="app-shell">
+                <Header />
+                <main className="app-main">
+                    <Outlet />
+                </main>
+                <Footer />
+            </div>
+        </UserContext.Provider>
+    );
 }
 
 export default App;
