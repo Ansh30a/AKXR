2614a29ab615221f9c2f7d11c0859bdb5d7c32c9
Ansh <anshuman302004@gmail.com>
2026-02-08 19:37:22 +0530

    learn/test: - advanced unit tests

--- FE-03/WEEK-2-5/FoodDeliveryApp/tests/Header.test.tsx
+++ FE-03/WEEK-2-5/FoodDeliveryApp/tests/Header.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { test, expect } from "vitest";
 import { MemoryRouter } from "react-router-dom";
 import Header from "../src/components/header/Header";
@@ -13,4 +13,26 @@ test("should render Header with Login", () => {
     );
 
     expect(screen.getByRole("button")).toBeInTheDocument();
+});
+
+test("should render Header with car items = 0", () => {
+    render(
+        <MemoryRouter>
+            <Provider store={appStore}><Header /></Provider>
+        </MemoryRouter>,
+    );
+
+    expect(screen.getByText("0")).toBeInTheDocument();
+});
+
+test("should change Login to Logout", () => {
+    render(
+        <MemoryRouter>
+            <Provider store={appStore}><Header /></Provider>
+        </MemoryRouter>,
+    );
+
+    fireEvent.click(screen.getByRole("button", { name: "Login" }));
+
+    expect(screen.getByRole("button", { name: "Default User" })).toBeInTheDocument();
 });
