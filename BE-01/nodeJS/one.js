6eedf208a74131f685a3bd10901adf64bda4af54
Ansh <anshuman302004@gmail.com>
2026-03-10 11:45:45 +0530

    learn: blocking / non-blocking code

--- BE-01/nodeJS/one.js
+++ BE-01/nodeJS/one.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+import fs from "fs";
 
 // ---------- Blocking Operation ----------
 // const res = fs.readFileSync("contacts.txt", "utf-8");
@@ -11,8 +11,16 @@ const fs = require("fs");
 // console.log(2);
 
 // ---------- Non-Blocking Operation ----------
-console.log(1);
-const res = fs.readFile("contacts.txt", "utf-8", (err, res) => {
-    console.log(res);
-});
-console.log(2);
+// console.log(1);
+// const res = fs.readFile("contacts.txt", "utf-8", (err, result) => {
+//     console.log(result);
+// });
+// console.log(res);   // -------- Confusion
+// console.log(2);
+
+// Default Thread Pool Size = 4
+// Max? - 8core CPU = 8
+
+import os from "os";
+
+console.log(os.cpus().length);
