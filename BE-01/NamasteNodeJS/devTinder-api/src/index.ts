145ee0e73ff9cce933efa682b1c49ddc0554b2d5
Ansh <anshuman302004@gmail.com>
2026-03-31 16:15:56 +0530

    learn: multiple route handlers, next(), errors in routing call stack and res.send()

--- BE-01/NamasteNodeJS/devTinder-api/src/index.ts
+++ BE-01/NamasteNodeJS/devTinder-api/src/index.ts
@@ -2,27 +2,16 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send(`Hello from the server.`);
-});
-
-app.get("/home", (req, res) => {
-    res.send(`Hello from the server home.`);
-});
-
-app.get("/test", (req, res) => {
-    res.send(`Hello from the server test.`);
-});
-
-app.post("/user", (req, res) => {
-    console.log(`Save data in the DB.`);
-    
-    res.send(`Data saved successfully.`);
-});
-
-app.post("/user", (req, res) => {
-    res.send({ firstName: "Anshuman", age: 21 });
-});
+app.use(
+    "/user",
+    (req, res, next) => {
+        // res.send(`response 1`);
+        next();
+    },
+    (req, res) => {
+        res.send(`response 2`);
+    },
+);
 
 app.listen(5000, () => {
     console.log(`Server running on 5000.`);
