1b21600b9d4297c2c4df0457b987d14626d6d974
Ansh <anshuman302004@gmail.com>
2026-03-31 13:04:22 +0530

    learn: basic express server

--- BE-01/NamasteNodeJS/devTinder-api/src/index.ts
+++ BE-01/NamasteNodeJS/devTinder-api/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send(`Hello from the server.`);
+});
+
+app.get("/home", (req, res) => {
+    res.send(`Hello from the server home.`);
+});
+
+app.get("/test", (req, res) => {
+    res.send(`Hello from the server test.`);
+});
+
+app.listen(5000, () => {
+    console.log(`Server running on 5000.`);
+});
