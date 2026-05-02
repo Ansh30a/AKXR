113612dd592e6e6738d6eb2865527a9b391e0346
Ansh <anshuman302004@gmail.com>
2026-05-03 00:15:35 +0530

    feat: implement pagination for user feed with limit and skip parameters

--- BE-01/NamasteNodeJS/devTinder-api/src/routes/user.route.ts
+++ BE-01/NamasteNodeJS/devTinder-api/src/routes/user.route.ts
@@ -73,6 +73,11 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user!;
 
+        const page = parseInt(req.query.page as string) || 1;
+        let limit = parseInt(req.query.limit as string) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+
         const allConnectionRequests = await ConnectionRequest.find({
             $or: [
                 { fromUserId: loggedInUser._id },
@@ -92,7 +97,12 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
                 { _id: { $nin: Array.from(hideUsersFromFeed) } },
                 { _id: { $ne: loggedInUser._id } },
             ],
-        }).select(USER_SAFE_DATA);
+        })
+            .select(USER_SAFE_DATA)
+            .skip(skip)
+            .limit(limit);
+
+        if (feedUsers?.$isEmpty) res.send("No users left to show.");
 
         res.send(feedUsers);
     } catch (err) {
