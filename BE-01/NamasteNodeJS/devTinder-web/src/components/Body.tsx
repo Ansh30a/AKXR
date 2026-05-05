7fb89bb3f4ec122117c358003e1518dea55a7582
Ansh <anshuman302004@gmail.com>
2026-05-05 15:54:12 +0530

    feat: refactor Body and Footer components for improved layout and styling

--- BE-01/NamasteNodeJS/devTinder-web/src/components/Body.tsx
+++ BE-01/NamasteNodeJS/devTinder-web/src/components/Body.tsx
@@ -4,9 +4,11 @@ import Footer from "./Footer";
 
 const Body = () => {
     return (
-        <div>
+        <div className="flex min-h-screen flex-col">
             <Navbar />
-            <Outlet />
+            <main className="flex-1">
+                <Outlet />
+            </main>
             <Footer />
         </div>
     );
